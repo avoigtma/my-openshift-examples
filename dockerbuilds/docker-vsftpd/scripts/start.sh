4b6879c66df18a2071a294f49c6525b6e604da62
Alberto Gonzalez <alberto.gonzalez@redhat.com>
2018-05-17 15:09:45 +0000

    Changing default directory

--- dockerbuilds/docker-vsftpd/scripts/start.sh
+++ dockerbuilds/docker-vsftpd/scripts/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/sbin/vsftpd  -olisten_port=2121 -orun_as_launching_user=true -oxferlog_file=/appScripts/xferlog -obackground=FALSE -opasv_enable=yes -opasv_min_port=21100 -opasv_max_port=21100 -opasv_address=127.0.0.1 
+/usr/sbin/vsftpd  -olisten_port=2121 -orun_as_launching_user=true -oxferlog_file=/appScripts/xferlog -obackground=FALSE -opasv_enable=yes -opasv_min_port=21100 -opasv_max_port=21100 -opasv_address=127.0.0.1 -olocal_root=/tmp
