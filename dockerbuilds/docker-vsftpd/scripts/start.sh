27e17db61c9128f7594abfd6ec024800429ff398
Alberto Gonzalez <alberto.gonzalez@redhat.com>
2018-05-17 13:51:44 +0000

    pasv ranges

--- dockerbuilds/docker-vsftpd/scripts/start.sh
+++ dockerbuilds/docker-vsftpd/scripts/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/sbin/vsftpd  -olisten_port=2121 -orun_as_launching_user=true -oxferlog_file=/appScripts/xferlog -obackground=FALSE -opasv_enable=NO
+/usr/sbin/vsftpd  -olisten_port=2121 -orun_as_launching_user=true -oxferlog_file=/appScripts/xferlog -obackground=FALSE -opasv_enable=yes -opasv_min=21100 -opasv_max=21100 -opasv_address=127.0.0.1 
